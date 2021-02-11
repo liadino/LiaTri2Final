d785ef90477289009a2679184fc78b54b1c147e6
lia.dino <lia.dino@MU-C1-15.mullen.pvt.k12.co.us>
2021-02-11 11:44:23 -0700

    new project commit

---
+++ VolleyballGuru/src/Defensive.java
@@ -0,0 +1,19 @@
+
+import liaFinal.Player;
+
+public class Defensive extends Player {
+ 	protected boolean gainPoints;
+  protected boolean runPlays;
+
+  @Override
+	private String purpose = " Prevent opponent from gaining points";
+  private String position = "Libero, Defensive Specialist";
+
+
+
+	
+	public returns() {
+		System.out.println("Nice dig!");
+
+
+}
---
+++ VolleyballGuru/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static int role;
+	public static Scanner input = new Scanner(System.in);
+	public static Player x;
+
+
+	public static void main(String[] args) {
+	     x = new Offensive();
+	      x.playVolleyball();
+	      x.getPurpose();
+	      x.getPosition();
+	      Offensive i = new Hitter();
+		    ((Hitter) i).getPurpose();
+	      ((Hitter) i).getPosition();
+
+
+	}
+	private void welcome() {
+		System.out.println("Hi! Welcome to the Volleybal Starter Assistant!");
+		System.out.println("Are you (1) a beginner player or (2) a beginner coach?");
+		 role = input.nextInt();
+	}
+
+}
